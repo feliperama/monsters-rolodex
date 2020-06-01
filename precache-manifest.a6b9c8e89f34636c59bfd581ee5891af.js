b83f6342024f66769a302b6a1e45c652e34a4553
feliperama <feliperama@gmail.com>
2020-06-01 12:49:03 -0400

    Updates

---
+++ precache-manifest.a6b9c8e89f34636c59bfd581ee5891af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a98266800664e11a87d3baadd99eccd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b5b8037f70e5d80f6d34",
+    "url": "/monsters-rolodex/static/css/main.e0c9d272.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b8037f70e5d80f6d34",
+    "url": "/monsters-rolodex/static/js/main.f7067273.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
